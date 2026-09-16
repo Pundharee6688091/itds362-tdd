37f20ba17ac4599e22abc3976e68dd58d8a2be57
Pundharee6688091 <pundharee.puk@student.mahidol.edu>
2026-09-16 21:58:39 +0700

    A5: green - add unit field, extract grams()/ounces() factories

--- test_kitchen.py
+++ test_kitchen.py
@@ -7,24 +7,24 @@
 # [ ] 200 g + 1 oz, reduced to grams, using a conversion rate
 # [ ] (200 g + 1 oz) x 2
 
-from kitchen import Quantity
+from kitchen import Quantity, grams, ounces
 
 def test_multiplication():
-    flour = Quantity(200)
-    assert flour.times(3) == Quantity(600)
+    flour = Quantity(200, "g")
+    assert flour.times(3) == Quantity(600, "g")
 
 def test_multiplication_by_two():
-    flour = Quantity(200)
-    assert flour.times(2) == Quantity(400)
+    flour = Quantity(200, "g")
+    assert flour.times(2) == Quantity(400, "g")
     
 def test_multiplication_returns_a_new_quantity():
-    flour = Quantity(200)
-    assert flour.times(3) == Quantity(600)
-    assert flour.times(2) == Quantity(400)
+    flour = Quantity(200, "g")
+    assert flour.times(3) == Quantity(600, "g")
+    assert flour.times(2) == Quantity(400, "g")
 
 def test_equality():
-    assert Quantity(200) == Quantity(200)
-    assert Quantity(200) != Quantity(300)
+    assert Quantity(200, "g") == Quantity(200, "g")
+    assert Quantity(200, "g") != Quantity(300, "g")
 
 def test_grams_are_not_ounces():
     assert Quantity(1, "g") != Quantity(1, "oz")
