9ed1c5266af29db76c8d853497e94a3031c0effb
Pundharee6688091 <pundharee.puk@student.mahidol.edu>
2026-09-16 22:05:30 +0700

    A7: red - test for cross-unit addition

--- test_kitchen.py
+++ test_kitchen.py
@@ -3,7 +3,7 @@
 # [x] multiplying a quantity does not modify the original
 # [x] two quantities with the same amount and unit are equal
 # [x] 1 oz is not the same as 1 g
-# [ ] 200 g + 300 g = 500 g
+# [x] 200 g + 300 g = 500 g
 # [ ] 200 g + 1 oz, reduced to grams, using a conversion rate
 # [ ] (200 g + 1 oz) x 2
 
@@ -33,3 +33,9 @@ def test_simple_addition():
     total = grams(200).plus(grams(300))
     converter = Converter()
     assert converter.reduce(total, "g") == grams(500)
+
+def test_reduce_with_conversion_rate():
+    total = grams(200).plus(ounces(1))
+    converter = Converter()
+    converter.add_rate("oz", "g", 28.35)
+    assert converter.reduce(total, "g") == grams(228.35)
